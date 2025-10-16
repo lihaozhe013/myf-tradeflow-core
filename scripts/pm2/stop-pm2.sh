e274bb34d2213de852a234a79b5efb92c2f444a2
Haozhe Li <lihaozhe013@outlook.com>
2025-10-15 21:08:46 -0400

    fix: now will delete process in stop script

--- scripts/pm2/stop-pm2.sh
+++ scripts/pm2/stop-pm2.sh
@@ -4,7 +4,9 @@ echo "Stopping the server..."
 
 if pm2 list | grep -q "myf-tradeflow-backend"; then
     pm2 stop myf-tradeflow-backend
-    echo "Server Stopped"
+    echo "Process Stopped"
+    pm2 delete myf-tradeflow-backend
+    echo "Process Deleted"
     
     echo "Current Status:"
     pm2 list
