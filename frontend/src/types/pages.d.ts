1bc0c27ad8e72c6a4c6c907fd60be47f66569287
Haozhe Li <lihaozhe013@outlook.com>
2025-10-16 11:33:37 -0400

    chore: removed useless path config

--- frontend/src/types/pages.d.ts
+++ frontend/src/types/pages.d.ts
@@ -1,10 +1,3 @@
-declare module './pages/*' {
-  import type { ComponentType } from 'react';
-
-  const Component: ComponentType<Record<string, unknown>>;
-  export default Component;
-}
-
 declare module '@/pages/*' {
   import type { ComponentType } from 'react';
 
